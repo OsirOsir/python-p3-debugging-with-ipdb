dd9aa84d6fda8d9964aaab8938beb61b1cb2e8c4
Philip osir Otieno <philiposir2@gmail.com>
2024-08-28 20:13:33 +0300

    updates

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -3,5 +3,6 @@
 import ipdb
 
 def plus_two(num):
-    num + 2
+    num = num + 2  # Update the num variable to reflect the addition of 2
+    # ipdb.set_trace()  # You can remove this line after confirming the function works correctly
     return num
